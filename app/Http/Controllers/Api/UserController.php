605c92ea5da1c9c2274b7010700305082db572db
Rambabu <78676139+ram8692@users.noreply.github.com>
2023-10-24 22:40:01 +0530

    all setup of jwt done

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,27 +5,61 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function register()
+
+    public function register(Request $request)
     {
-        //
+
+        //dd(55);
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required',
+            'phone_no' => 'required',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->phone_no = $request->phone_no;
+        $user->save();
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'status' => 1
+        ], 200);
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function login()
+
+    public function login(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!$token = auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json(['message' => 'Unauthorized','status'=>0], 401);
+        }
+
+        //get value of token
+        //i dont want true or false i want value of token
+        dd(auth()->user());
+        dd($token);
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'status' => 1,
+            'message' => 'Login Successful'
+        ]);
+
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CourseController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
